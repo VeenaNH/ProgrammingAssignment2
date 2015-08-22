7e0717e4ca03c39f46f0e98fae637828ded2f232
VeenaNH <veenanh1305@gmail.com>
2015-08-22 19:11:38 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+inverse<-function(x=matrix()) {
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	if(nrow(x)!=ncol(x)) 
+		return("Not a square matrix")
+	else {
+		y=solve(x)
+		return(y) }
 }
