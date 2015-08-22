f3208350d027b8660d0b4bb2d7926abccf3526b1
VeenaNH <veenanh1305@gmail.com>
2015-08-22 19:31:15 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,20 @@
+CacheMatrix <- function(x = matrix()) {
+              inv = NULL
+        set = function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get = function() x
+        setinv = function(inverse) inv <<- inverse 
+        getinv = function() inv
+        return(x)
+}
+
 inverse<-function(x=matrix()) {
 
 	if(nrow(x)!=ncol(x)) 
 		return("Not a square matrix")
 	else {
 		y=solve(x)
-		return(y) }
+return(y) }
 }
